f0772dacf3ea3e201787798b493ebe93365d677b
Lianghai-Yang <yanglianghai1994@gmail.com>
2019-12-07 17:03:07 -0500

    fix setting button

--- client/pages/map.js
+++ client/pages/map.js
@@ -159,7 +159,7 @@ class Map extends React.Component {
     sideIconRight() {
         return (
             //<FaBars color="#007bff" size="1.5rem" onClick={() => alert('testing')} className="flex-grow-0" />
-            <FaBars color="#007bff" size="1.5rem" onClick={() => this.props.router.replace('/setting')} className="flex-grow-0" />
+            <FaBars color="#007bff" size="1.5rem" onClick={() => this.props.router.push('/setting')} className="flex-grow-0" />
         )
     }
 
